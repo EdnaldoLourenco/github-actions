e90ba6da576c15fbc6707f48546fe35bc0e33706
Ednaldo Lourenço <142667824+EdnaldoLourenco@users.noreply.github.com>
2025-12-12 22:02:32 -0300

    adjust path level

---
+++ iac/variables.tf
@@ -0,0 +1,13 @@
+variable "ami_id" {
+  type = string  
+  default = "ami-005e54dee72cc1d00"
+}
+variable "instance_type"  {
+  type = string
+  default = "t3.micro"
+
+}
+variable "variavel_inexistente" {
+  type = string
+  default = "valor_inexistente"
+}
